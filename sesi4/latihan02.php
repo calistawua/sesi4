6600037349441f68b7263b41437a6e810314c808
calistawua <127809356+calistawua@users.noreply.github.com>
2023-04-04 12:28:15 +0800

    Add files via upload

---
+++ sesi4/latihan02.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Data</title>
+</head>
+<body>
+
+<?php
+
+     if(isset($_GET["txUSER"])){
+        $usr = $_GET["txUSER"];
+        if($usr==""){
+             echo "<div><h3 style='color:red;padding:5px;'>User Name/password salah/</h3></div>";
+    }
+
+}
+
+?>
+    
+    <form action ="latihan02.php" method ="POST">
+        <div>
+            User name
+            <input type="text"id ="txUSER" name="txUSER">
+            <div>
+
+            <div>
+                password 
+                <input type="password"id ="txPASKEY" name="txPASSKEY">
+            <div>
+
+            <div>
+                <button type="submit"> Login </button>
+                <a href="daftar.php"> Daftar </button>
+            <div>
+
+</form>
+
+</body>
+</html>
